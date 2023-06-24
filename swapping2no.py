ca279a980bc06bba8a0e3d52e35ea6506a88a1ba
Sisirapuppala <47064932+Sisirapuppala@users.noreply.github.com>
2023-06-25 00:38:46 +0530

    Create swapping2no.py

---
+++ swapping2no.py
@@ -0,0 +1,7 @@
+'''Swapping 2 numbers'''
+a=int(input("before swapping first number a: "))
+b=int(input("before swapping second number b: "))
+temp=a
+a=b
+b=temp
+print("after swapping a=",a,"after swapping b=",b)
