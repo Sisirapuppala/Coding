d9468c09bcba10ef874bc918a01df4dec2a88b2d
Sisirapuppala <47064932+Sisirapuppala@users.noreply.github.com>
2023-07-25 19:15:40 +0530

    SumofNnumbers.py

---
+++ SumofNnumbers.py
@@ -0,0 +1,11 @@
+Find the Sum of the First N Natural Numbers in Python
+Method 1: Loop
+num = int(input("Enter Value:"))
+sum = 0
+for i in range(num+1):
+  sum+=i
+print(sum)
+
+Method 2: Formula
+Num= int(input("Enter number"))
+print(int( Num * ( Num + 1 ) ) / 2)
