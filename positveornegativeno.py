b7bd571a63388a22cc25d8879c0dba22f7598748
Sisirapuppala <47064932+Sisirapuppala@users.noreply.github.com>
2023-07-25 19:03:47 +0530

    Create positveornegativeno.py

---
+++ positveornegativeno.py
@@ -0,0 +1,10 @@
+Python Program to check if a Number Is Positive Or Negative
+
+a= int(input("Enter number: "))
+if a>0:
+    print("Positive Number")
+elif a==0:
+    print("Zero")
+    
+else:
+    print("Negative Number")
