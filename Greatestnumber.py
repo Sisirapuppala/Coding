c0c74ecec56a873a5c41203272616e21f5398a89
Sisirapuppala <47064932+Sisirapuppala@users.noreply.github.com>
2026-09-13 16:23:13 +0530

    Create Greatestnumber.py

---
+++ Greatestnumber.py
@@ -0,0 +1,9 @@
+a= int(input("enter number 1:"))
+b= int(input("enter number 2:"))
+if(a>b):
+  print(f"{a} is greater than {b}")
+elif (a<b):
+  print(f"{b} is greater than {a}")
+else:
+  print("both numbers are same")
+
